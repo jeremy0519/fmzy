30ec3ad04dd0249b7db1e43157857e04e329cfd2
tzp <fushantangzp201403@126.com>
2022-06-29 01:22:45 +0800

    936

---
+++ zhuxian/js/0-1-1.js
@@ -0,0 +1,49 @@
+$(document).ready(function () {
+    Parse.initialize("fmzy")
+    Parse.serverURL = 'http://43.142.126.163:1337/parse'
+    const classData = Parse.Object.extend("fmzy")
+    function log(text) {
+        var d = new Date()
+        const element = document.createElement('p')
+        element.innerHTML = [
+            `<p class="text-start text-break">`,
+            `${d}`,
+            `: `,
+            `${text}`,
+            `</p>`].join("")
+        $("#logArea").append(element)
+    }
+    log("数据库初始化完毕")
+    log("936936")
+    const alertPlaceholder = $('#liveAlertPlaceholder')
+    function alertError(message) {
+        if (document.getElementById("closebtn")) {
+            $("#closebtn").trigger("click")
+        }
+        const wrapper = document.createElement('div')
+        wrapper.innerHTML = [
+            `<div class="alert ms-5 me-5 fade show alert-danger alert-dismissible d-flex align-items-center" role="alert">`,
+            `   <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Danger:"><use xlink:href="#exclamation-triangle-fill"/></svg>`,
+            `   <div>${message}</div>`,
+            '   <button id="closebtn" type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+            '</div>'
+        ].join('')
+        alertPlaceholder.append(wrapper)
+    }
+    
+    function alertSuccess(message) {
+        if (document.getElementById("closebtn")) {
+            $("#closebtn").trigger("click")
+        }
+        const wrapper = document.createElement('div')
+        wrapper.innerHTML = [
+            `<div class="alert ms-5 me-5 fade show alert-success alert-dismissible d-flex align-items-center" role="alert">`,
+            `   <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Success:"><use xlink:href="#check-circle-fill"/></svg>`,
+            `   <div>${message}</div>`,
+            '   <button id="closebtn" type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+            '</div>'
+        ].join('')
+        alertPlaceholder.append(wrapper)
+    }
+
+})
