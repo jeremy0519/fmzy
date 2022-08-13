69bde0de3f05355ab4fc7cdfb0e970ef16386929
jeremy <jeremy@jeremy.com>
2022-08-13 21:51:56 +0800

    936

--- js/ggb.js
+++ js/ggb.js
@@ -1,5 +1,8 @@
 var params = {"appName": "geometry", "width": 800, "height": 600, "showToolBar": true, "showAlgebraInput": true, "showMenuBar": true };
 var applet = new GGBApplet(params, true);
+var params2 = {"appName": "3d", "width": 800, "height": 600, "showToolBar": true, "showAlgebraInput": true, "showMenuBar": true };
+var applet2 = new GGBApplet(params2, true);
 window.addEventListener("load", function() { 
     applet.inject('ggb-element');
+    applet2.inject('ggb-element2');
 });
